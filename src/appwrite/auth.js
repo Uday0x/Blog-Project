6176dd10a8643bc648f47d7a519d9b3d5bf63744
uuuuday <krishnauday200301@gmail.com>
2025-08-29 11:09:38 +0530

    add the auth.js snippet

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -15,8 +15,10 @@ import { Client, Account, ID } from "appwrite";
 //     'password'
 // );
 
-//if required same code of above can be written we are using another approach for smotther operation 
+//if required same code of above can be written we are using another approach for smoother operation
 
+//we are class syntax so that we remove teh concept of vendor lock-in 
+//after this we can easily switch to firebase ,supabase (backend services)
 
 export class Authservice{
     client = new Client();
@@ -30,6 +32,73 @@ export class Authservice{
         .setProject(config.APPWRITE_PROJECT_ID)
         this.account = new Account(this.client);
     }
+
+
+    //now creating the methods inside the Authservice class so that if changes are required in the future, we can easily manage them
+
+    //user will pass a object of email,passowrd,name thats y we destrcuturing it here
+    async createAccount({email,password,name}){
+        try {
+            const user = await this.account.create(
+                ID.unique(),
+                email,
+                password,
+                name
+            );
+
+
+            if(!user){
+                //or you can call another  method //login method 
+                return this.Login({email,password})
+            }else{
+                return user
+            }
+            
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    //
+    async Login({email,password}){
+        //safe to wrap in it try catch 
+        try {
+            const loggedInUser =this.account.createEmailPasswordSession(
+                email,
+                password
+            )
+
+
+            if(!loggedInUser){
+                return "user not found"
+            }else{
+                return loggedInUser
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrtite service :: getcurrentuser error",error)
+        }
+        return null;
+    }
+
+
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite error :: Logout error",error)
+        }
+    }
+
+    
 }
 
 
