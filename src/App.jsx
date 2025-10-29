a73c6eaa70f5f42e725dc1f2188d354f6b18cc98
uuuuday <krishnauday200301@gmail.com>
2025-10-29 12:38:59 +0530

    add the code basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { useDispatch } from 'react-redux';
+import {login,logout} from "./store/authSlice.js"
+import authservice from './appwrite/auth.js';
+import { Footer, Header } from './components/index.js';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
+
+
+  const [loading,setloading] =useState(true)
+
+  useEffect(()=>{
+      authservice.getCurrentUser()
+        .then((userData)=>{  //u can use superman in place of userData
+          if(userData){
+            dispatch(login(userData))
+          } else{
+            dispatch(logout())
+          }
+        })
+        .finally(()=>setloading(false))
+  },[])
+
+
+  return !loading ? (
+    <>
+      <Header/>
+      <main>
+        <Outlet/>  //ToDo
+      </main>
+      <Footer/>
+   </>
+  ) : (
+    null
   )
+  
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Footer from "./Footer/Footer.jsx"
+import Header from "./Header/Header.jsx"
+
+
+export  {
+    Footer,
+    Header
+}
