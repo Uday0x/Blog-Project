35056ecd13be2aee938ad333e99357c974afa728
uuuuday <krishnauday200301@gmail.com>
2025-11-29 23:53:15 +0530

    add the code of login

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,12 @@ import Footer from "./Footer/Footer.jsx"
 import Header from "./Header/Header.jsx"
 import Container from "./Container/Container.jsx"
 import Logo from "./Logo.jsx"
-
+import Input from "./Input.jsx"
 
 export  {
     Footer,
     Header,
     Container,
     Logo,
+    Input
 }
