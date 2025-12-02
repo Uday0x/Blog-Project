3f5e3b57a5ade8469fa207cda9c8142750027daf
uuuuday <krishnauday200301@gmail.com>
2025-12-02 15:34:45 +0530

    add the postForm code

--- src/components/index.js
+++ src/components/index.js
@@ -3,11 +3,15 @@ import Header from "./Header/Header.jsx"
 import Container from "./Container/Container.jsx"
 import Logo from "./Logo.jsx"
 import Input from "./Input.jsx"
+import Select from "./Select.jsx"
+import RTE from "./RTE.jsx"
 
 export  {
     Footer,
     Header,
     Container,
     Logo,
-    Input
+    Input,
+    Select,
+    RTE
 }
